f705801a8f0fa93c1d3a14cd9cab7a011eb488c7
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-26 18:26:21 +0100

    asddsa

--- src/com/hastingsdirect/sql/CMDBConnection.groovy
+++ src/com/hastingsdirect/sql/CMDBConnection.groovy
@@ -21,7 +21,8 @@ class CMDBConnection {
 		def cl=Class.forName("org.h2.Driver")
 		println "class for name:"+cl
 		CMDBConnection conn=new CMDBConnection()
-		return groovy.sql.Sql.newInstance(conn.url,conn.user,conn.password,conn.driver)
+		return null;
+//		return groovy.sql.Sql.newInstance(conn.url,conn.user,conn.password,conn.driver)
 	}
 
 	static void execute(Closure c) {
