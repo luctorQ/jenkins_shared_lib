6261c2116eaf23a8074cb494b4ad0fe7aaaa32cf
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 11:11:57 +0100

    updated values

--- vars/jsonParam.groovy
+++ vars/jsonParam.groovy
@@ -1,6 +1,8 @@
 import com.hastingsdirect.ep.ExtendedProperties
 
-def call(String name,String description,String groovyScript) {
-	return ExtendedProperties.createJsonField(name,groovyScript,description)
+def call(Map map=[:]) {
+	def defaultMap = [name:'JSON_PARAM',groovyScript:'',description:'']
+	defaultMap<<map
+	return ExtendedProperties.createJsonField(defaultMap.name,defaultMap.groovyScript,defaultMap.description)
 }
 
