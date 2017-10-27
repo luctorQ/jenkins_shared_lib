fae9c6604cf12437c1491215dd82f42699684b13
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 14:06:13 +0100

    dd

--- src/com/hastingsdirect/ep/PromotedBuildsExt.groovy
+++ src/com/hastingsdirect/ep/PromotedBuildsExt.groovy
@@ -3,6 +3,9 @@ import groovy.lang.GroovyCodeSource;
 
 class PromotedBuildsExt extends ExtendedProperty{
 
+	PromotedBuildsExt(String name,String description='',String sharedlibrarygroovyclasspath="c:/tmp/libs_jenkins_global/jenkins_shared_lib.jar"){
+		super(name,description,sharedlibrarygroovyclasspath)
+	}
 	String groovyScript() {
 		return groovyScriptFromCodeSource("scripts/PromotedBuildsExtScript.groovy");
 	}
