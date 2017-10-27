8c6c612aeb4c60e77e3b3cae7449cfe0e213641d
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 07:56:47 +0100

    fds

--- src/com/hastingsdirect/vo/PromotedBuild.groovy
+++ src/com/hastingsdirect/vo/PromotedBuild.groovy
@@ -17,4 +17,27 @@ class PromotedBuild implements Serializable {
 	String jobstatus
 	Date created_at
 	Date updated_at
+
+	public PromotedBuild(
+	Integer id
+	,String branch
+	,Integer cijenkinsbuildid
+	,Integer abjenkinsbuildid
+	,Integer absvnrevisionnumber
+	,Integer bcjenkinsbuildid
+	,Integer bcsvnrevisionnumber
+	,Integer ccjenkinsbuildid
+	,Integer ccsvnrevisionnumber
+	,Integer pcjenkinsbuildid
+	,Integer pcsvnrevisionnumber
+	,String junitstatus
+	,String smoketeststatus
+	,String jobstatus
+	,Date created_at
+	,Date updated_at) {
+		this.id=id
+		this.branch=branch
+		this.created_at=created_at
+		this.updated_at=updated_at
+	}
 }
