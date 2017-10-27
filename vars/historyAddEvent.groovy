92003147b3c9cced2bc534da08598ebc16a05283
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 17:43:06 +0100

    sd

--- vars/historyAddEvent.groovy
+++ vars/historyAddEvent.groovy
@@ -2,8 +2,8 @@ import com.hastingsdirect.pipeline.history.HistoryContext
 
 def call() {
 	println 'thisddd:'+this
-	if(historyContext!=null) {
-		historyContext.
+	if(historyContext==null) {
+		historyContext=new HistoryContext()
 	}
 	historyContext.addEvent(msg)
 }
