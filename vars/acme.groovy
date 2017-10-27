4dbaece24ae934e54774b839df18bd1712b8913f
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 18:38:49 +0100

    sd

--- vars/acme.groovy
+++ vars/acme.groovy
@@ -1,4 +1,5 @@
 class acme implements Serializable {
+	private List history
 	private String name
 	def setName(value) {
 		name = value
@@ -6,7 +7,16 @@ class acme implements Serializable {
 	def getName() {
 		name
 	}
+	
 	def caution(message) {
 		echo "Hello, ${name}! CAUTION: ${message}"
+	}
+	
+	def addEvent(String msg) {
+		history<<msg
+	}
+	
+	def getHistory() {
+		return history
 	}
 }
