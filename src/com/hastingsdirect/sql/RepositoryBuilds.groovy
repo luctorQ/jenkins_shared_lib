a44fbafebb0004c2c0dbf0d603df0a99395a6f38
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 08:19:54 +0100

    dsas

--- src/com/hastingsdirect/sql/RepositoryBuilds.groovy
+++ src/com/hastingsdirect/sql/RepositoryBuilds.groovy
@@ -8,11 +8,16 @@ class RepositoryBuilds implements Serializable{
 	public List<PromotedBuild> buildsOnePromoted() {
 		Sql sql=CMDBConnection.createConnection();
 		List<PromotedBuild> promoted=[]
-		sql.eachRow('select * from onebuildpromotion'){ row->
+		def rows=sql.rows('select * from onebuildpromotion')
+		rows.each({row->
+			promoted<<row as PromotedBuild
+		})
+		
+/*		sql.eachRow('select * from onebuildpromotion'){ row->
 			println 'rrrrow:'+row
 			promoted<<row[0] as PromotedBuild
 		}
-
+*/
 		sql.close()
 		return promoted;
 	}
