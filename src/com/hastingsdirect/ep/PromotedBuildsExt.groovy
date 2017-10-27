2bf450081b98f69cd681a8f59ca893a8648389c1
PLUSZYNSKI <pluszynski@bleak.pl>
2017-10-27 14:03:22 +0100

    dssd

--- src/com/hastingsdirect/ep/PromotedBuildsExt.groovy
+++ src/com/hastingsdirect/ep/PromotedBuildsExt.groovy
@@ -3,7 +3,6 @@ import groovy.lang.GroovyCodeSource;
 
 class PromotedBuildsExt extends ExtendedProperty{
 
-	
 	String groovyScript() {
 		return groovyScriptFromCodeSource("scripts/PromotedBuildsExtScript.groovy");
 	}
