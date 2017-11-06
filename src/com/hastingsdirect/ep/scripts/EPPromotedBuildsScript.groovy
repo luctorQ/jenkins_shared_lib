61b30cbd06c945c69c2b1d3dab15fafebf6290a9
pluszynski <pluszynski@bleak.pl>
2017-11-06 14:54:18 +0000

    removed fetchDBInfo

--- src/com/hastingsdirect/ep/scripts/EPPromotedBuildsScript.groovy
+++ src/com/hastingsdirect/ep/scripts/EPPromotedBuildsScript.groovy
@@ -6,7 +6,7 @@ import com.hastingsdirect.sql.RepositoryBuilds;
 def repo=new RepositoryBuilds()
 def builds = repo.buildsOnePromoted()
 
-def dbInfo=repo.fetchDBInfo()
+//def dbInfo=repo.fetchDBInfo()
 
 def jsonEditorOptions = ExtendedProperty.fromJson(/{
 	   disable_edit_json: true,
