c1d650bf39c80f8b4c65836d470c482bd1232b71
pluszynski <pluszynski@bleak.pl>
2017-11-19 20:31:29 +0000

    dsa

--- vars/eventsRestore.groovy
+++ vars/eventsRestore.groovy
@@ -1,6 +1,10 @@
+import org.boon.Boon
+import org.boon.json.JsonFactory;
+
 def call(build) {
 	def j1EnvVariables = build.buildVariables;
 	println 'ext env vairalbles:'+j1EnvVariables
 	def extHistory=j1EnvVariables.EVENTS_HISTORY
-	return extHistory
+	def value=Boon.fromJson(jsonString)
+	return value
 }
--- vars/eventsStore.groovy
+++ vars/eventsStore.groovy
@@ -1,3 +1,5 @@
+import org.boon.json.JsonFactory;
+
 def call() {
 	println 'eventsStore class:'+this.getClass()
 	println 'eventsStore superclass:'+this.getClass().getSuperclass()
@@ -8,12 +10,14 @@ def call() {
 	println 'binding variables:'+this.getBinding().getVariables()
 	println 'binding steps:'+this.getBinding().getVariables().steps
 	
-	println 'allmethods:'+this.metaClass.methods*.name.sort().unique()
-	
-	println 'env.pipelineEvents:'+env.pipelineEvents
-	pipelineEvents.add('inscript add')
-	
 	events.add('abc')
-	env.EVENTS_HISTORY=events.list
+	
+	List restored=eventsRestore()
+	
+	restored.addAll(events.list)
+	
+	println 'restored:'+restored
+	
+	env.EVENTS_HISTORY=JsonFactory.toJson(restored)
 	println 'inside events history:'+events.list
 }
