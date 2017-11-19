baa1aa847b84ce2ec1271ec1fbffac9122b982b2
pluszynski <pluszynski@bleak.pl>
2017-11-19 19:28:11 +0000

    sd

--- vars/eventsStore.groovy
+++ vars/eventsStore.groovy
@@ -2,6 +2,8 @@ def call() {
 	
 	println 'eventsStore this:'+this.getClass()
 	println 'eventsStore this:'+this
+	println 'events:'+this.events
+	
 	events.add('abc')
 	env.EVENTS_HISTORY=events.list
 	println 'inside events history:'+events.list
