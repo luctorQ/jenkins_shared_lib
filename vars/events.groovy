bbae6c241f8be864ef0aa00b857e5bed29fb613c
pluszynski <pluszynski@bleak.pl>
2017-11-19 21:00:17 +0000

    sd

--- vars/events.groovy
+++ vars/events.groovy
@@ -1,6 +1,8 @@
 class events implements Serializable {
 	private List history=[]
-	
+	public events(scr) {
+		println 'events pr:'+scr
+	}
 	def add(String msg) {
 		history<<[msg:msg,date:new Date()]
 	}
