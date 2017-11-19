96b2b33865b3409790d84c20523b5d168da75f71
pluszynski <pluszynski@bleak.pl>
2017-11-19 22:23:26 +0000

    sfd

--- vars/eventsRestore.groovy
+++ vars/eventsRestore.groovy
@@ -1,11 +1,16 @@
 import org.boon.Boon
-import org.boon.json.JsonFactory;
+import org.boon.json.JsonFactory
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
-
-def call(build) {
+def call(RunWrapper build) {
 	def j1EnvVariables = build.buildVariables;
-	println 'ext env vairalbles:'+j1EnvVariables
 	def extHistory=j1EnvVariables.EVENTS_HISTORY
+	def value=Boon.fromJson(extHistory)
+	return value
+}
+
+def call() {
+	def extHistory=env.EVENTS_HISTORY
 	def value=Boon.fromJson(extHistory)
 	return value
 }
