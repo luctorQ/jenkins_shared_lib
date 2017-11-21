e4a933db5ff03813a6cf9a521044e7696a4d609a
pluszynski <pluszynski@bleak.pl>
2017-11-21 22:12:42 +0000

    dsa

--- vars/eventsStore.groovy
+++ vars/eventsStore.groovy
@@ -4,7 +4,7 @@ import org.boon.Boon
 import org.boon.json.JsonFactory;
 
 def convert(val) {
-	println 'v:'+val+' c:'+val?val.getClass():null
+	println 'convert v:'+val+' c:'+val?val.getClass():null
 	if(val && val instanceof GString) {
 		return val.toString()
 	}else {
