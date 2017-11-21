6789649f5e09fe841d59b6cbfbba3d5ee63114e9
pluszynski <pluszynski@bleak.pl>
2017-11-21 20:51:09 +0000

    sdaf

--- vars/eventsStore.groovy
+++ vars/eventsStore.groovy
@@ -29,46 +29,8 @@ def call(String msg,String type='GENERAL') {
 	env.EVENTS_HISTORY=JsonFactory.toJson(allEvents)
 }
 
-def call(eventsList) {
-	script{
-	println 'eventsStore class:'+this.getClass()
-	println 'eventsStore superclass:'+this.getClass().getSuperclass()
-	println 'eventsStore this:'+this
-	println 'events this:'+this.events
-	println 'events:'+events
-	
-	println 'binding variables:'+this.getBinding().getVariables()
-	println 'binding steps:'+this.getBinding().getVariables().steps
-	
-	events.add('abc')
-	
-	def restoredHistory=eventsRestore()
-//	def restoredHistory=[]
-	println('store restoredHistory:'+restoredHistory)
-	println('store eventsList :'+eventsList)
-	println('store events.list :'+events.list)
-	
-	def restored=[restoredHistory,eventsList,events.list].flatten()
-	println('storeAll:'+restored)
-	env.EVENTS_HISTORY=JsonFactory.toJson(restored)
-
-		
-//	return restored
-	/*
-	def eventsHistory=env.EVENTS_HISTORY?:'[]'
-	def mapper =  JsonFactory.create();
-	def restored1=mapper.readValue(eventsHistory,List.class,Event.class)
-	println 'restored%%%'+restored1
-//	mapper=null;
-	def restored=Boon.fromJson(eventsHistory)
-	restored=[restored1,restored, eventsList,events.list].flatten()
-	
-	println 'restored:'+restored
-	
-	env.EVENTS_HISTORY=JsonFactory.toJson(restored)
-	
-	println('env.events_History after toJson:'+env.EVENTS_HISTORY)
-	println 'inside events history:'+events.list
-}*/
-	}
+def call(List eventsList) {
+	def allEvents=eventsRestore()
+	def combinedEvents=[allEvents,eventsList].flatten()
+	env.EVENTS_HISTORY=JsonFactory.toJson(combinedEvents)
 }
