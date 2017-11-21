282a226f282f607634d3f3fd7c6460841d28f660
pluszynski <pluszynski@bleak.pl>
2017-11-21 17:49:48 +0000

    dsaf

--- vars/events.groovy
+++ vars/events.groovy
@@ -1,21 +1,17 @@
 class events implements Serializable {
 	private List history=[]
 
-	def add(String msg,String type='Default') {
+	def add(String msg,String type='GENERAL') {
 		this.addNamed(msg:msg,type:type)
 	}
 
-	def addNamed(Map params) {
+	def add(Map params) {
 		def eventData=[
 			msg:params.msg,
 			date:new Date(),
 			type:params.type?:'GENERAL',
 			ref:params.ref
 		];
-
-		//		eventData[date:new Date()]
-		//		eventData[date:new Date(), type:params[type]?:'GENERAL']
-		println ('event Data:'+eventData)
 		history<<eventData
 	}
 
