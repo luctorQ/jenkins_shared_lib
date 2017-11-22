63170b5562f76782a9f78d0c0810fe14e89ce733
pluszynski <pluszynski@bleak.pl>
2017-11-22 16:42:35 +0000

    sfda

--- src/templates/testwithlayout.groovy
+++ src/templates/testwithlayout.groovy
@@ -1,3 +1,3 @@
-layout 'templates/layout-main.tpl',                                   
+layout 'templates/layout-main.groovy',                                   
     title: 'Layout example',                                
     bodyContents: contents { p('This is the body') }   
