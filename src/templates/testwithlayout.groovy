8b93581af866ba7547ea6546393c9b12e5ca9956
pluszynski <pluszynski@bleak.pl>
2017-11-22 17:19:51 +0000

    ds

--- src/templates/testwithlayout.groovy
+++ src/templates/testwithlayout.groovy
@@ -1,18 +1,32 @@
 layout 'templates/layouts/layout-email.groovy',
 title: 'email',
 bodyContents: contents {
-	
+
 	TEST?p('TEST exists'):p('no TEST')
-	
-	APP_BUILD_DONE?h3('CREATED APPS'):p('No new crated builds')
-/*	APP_BUILD_DONE.each{
-		fragment 
-	}
-	pc: 26186 svn:Hastings/branches/CAD7/PolicyCenter/modules/configuration
-	status:UNSTABLE
-	build job: http://bx-cinappd03.network.uk.ad:8080/job/PC%20Build/609/
-	JUnit Tests TOTAL:195 FAILED:7
-*/	
+
+	//	APP_BUILD_DONE?h3('CREATED APPS'):p('No new crated builds')
+
+	APP_BUILD_DONE?div{
+		h3('CREATED APPS')
+		table{
+			APP_BUILD-DONE.each{
+				row{
+					td(it.appname)
+					td(it.buildidentifier)
+				}
+			}
+
+		}
+	}:p('No new crated builds')
+
+	/*	APP_BUILD_DONE.each{
+	 fragment 
+	 }
+	 pc: 26186 svn:Hastings/branches/CAD7/PolicyCenter/modules/configuration
+	 status:UNSTABLE
+	 build job: http://bx-cinappd03.network.uk.ad:8080/job/PC%20Build/609/
+	 JUnit Tests TOTAL:195 FAILED:7
+	 */	
 	p('This is the body')
 	p('new body')
 }
