232133a23196e7cd7062141aa6d9c45103dfcdf7
pluszynski <pluszynski@bleak.pl>
2017-11-22 13:58:24 +0000

    dsa

---
+++ src/com/hastingsdirect/templates/Template.groovy
@@ -0,0 +1,17 @@
+package com.hastingsdirect.vo
+
+class Template implements Serializable {
+
+	String createTemplate() {
+		yieldUnescaped '<!DOCTYPE html>'
+		html(lang:'en') {
+			head {
+				meta('http-equiv':'"Content-Type" content="text/html; charset=utf-8"')
+				title('My page')
+			}
+			body {
+				p('This is an example of HTML contents')
+			}
+		}
+	}
+}
