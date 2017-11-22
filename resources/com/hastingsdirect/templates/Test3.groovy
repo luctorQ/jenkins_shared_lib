ef44845a5116ff353f49bd84cdf9aba57396489a
pluszynski <pluszynski@bleak.pl>
2017-11-22 15:23:07 +0000

    ds

--- resources/com/hastingsdirect/templates/Test3.groovy
+++ resources/com/hastingsdirect/templates/Test3.groovy
@@ -3,10 +3,12 @@ html(lang:'en') {
 		meta('http-equiv':'"Content-Type" content="text/html; charset=utf-8"')
 		title('My page')
 		style{
+			yieldUnescaped """ 
 			p {
 				color: blue;
 				font-size: 46px;
 			  }
+			"""
 		}
 	}
 	body { p('This is an example of HTML contents') }
