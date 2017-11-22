b90d575134cdf4d0522dddf87ea37662773e9e7b
pluszynski <pluszynski@bleak.pl>
2017-11-22 17:06:54 +0000

    ds

--- src/templates/testwithlayout.groovy
+++ src/templates/testwithlayout.groovy
@@ -1,6 +1,18 @@
-layout 'templates/layouts/layout-main.groovy',
+layout 'templates/layouts/layout-email.groovy',
 title: 'email',
 bodyContents: contents {
+	
+	APP_BUILD_DONE?{
+		h3('CREATED APPS')	
+	}:p('No new crated builds')
+/*	APP_BUILD_DONE.each{
+		fragment 
+	}
+	pc: 26186 svn:Hastings/branches/CAD7/PolicyCenter/modules/configuration
+	status:UNSTABLE
+	build job: http://bx-cinappd03.network.uk.ad:8080/job/PC%20Build/609/
+	JUnit Tests TOTAL:195 FAILED:7
+*/	
 	p('This is the body')
 	p('new body')
 }
--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -2,14 +2,33 @@ import com.hastingsdirect.templates.Template
 
 import groovy.text.markup.MarkupTemplateEngine
 import groovy.text.markup.TemplateConfiguration
+import com.hastingsdirect.ep.ExtendedProperty
 
+def HISTORY_EVENTS_JSON="""
+[{
+		"date":1511369584479,"msg":"Build 621 of pc completed with result UNSTABLE","ref":{
+			"build":{
+				"appname":"pc","artifactoryurl":"","buildidentifier":"26294","buildresult":"UNSTABLE","created_at":1511349799117,"createdate":1511349799000,"deleted":false,"description":"","id":1870,"important":false,"jenkinsbuildnumber":621,"jenkinsbuildurl":"http://bx-cinappd03.network.uk.ad:8080/job/PC%20Build/621/","storedinartifactory":false,"svnpath":"Hastings/branches/CAD7/PolicyCenter/modules/configuration","svnrevisionnumber":26294,"trunk":false,"updated_at":1511349799117
+			},"junittests":{
+				"failCount":4,"failureDiffString":" / -10","skipCount":0,"testsUrl":"http://bx-cinappd03.network.uk.ad:8080/job/PC%20Build/621//testReport","totalCount":195
+			}
+		},"type":"APP_BUILD_DONE"
+	}]
+"""
 
 def call(String templatepath) {
 	println 'workspace:'+WORKSPACE
 	println 'templatepath:'+templatepath
 	println 'pwd:'+pwd()
 	def mytemplate=new Template('gogo')
-	def str=mytemplate.eval('templates/testwithlayout.groovy')
+	def HISTORY_EVENTS=ExtendedProperty.fromJson(HISTORY_EVENTS_JSON)
+
+	def bindings=[
+		APP_BUILD_DONE:HISTORY_EVENTS.findAll({it.type=='APP_BUILD_DONE'})
+	]
+
+
+	def str=mytemplate.eval('templates/testwithlayout.groovy',bindings)
 
 	return str
 }
