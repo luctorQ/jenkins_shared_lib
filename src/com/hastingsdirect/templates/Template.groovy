78def7369d3c44aa9d9923b3f585615a741af147
pluszynski <pluszynski@bleak.pl>
2017-11-22 14:21:33 +0000

    afd

--- src/com/hastingsdirect/templates/Template.groovy
+++ src/com/hastingsdirect/templates/Template.groovy
@@ -2,6 +2,13 @@ package com.hastingsdirect.vo
 
 class Template implements Serializable {
 	
+	
+	String evalTemplate(template) {
+		println 'evalTemplate:'+template;
+		def shell=new GroovyShell()
+		
+	}
+	
 	String templateScriptFromCodeSource(String templateFileName) {
 		def is=new InputStreamReader(this.getClass().getResourceAsStream(templateFileName))
 		def gsc=new GroovyCodeSource(is,'script_name_dynamic','UTF-8')
---
+++ vars/sendEmail.groovy
@@ -0,0 +1,7 @@
+import com.hastingsdirect.templates.Template
+
+def call(){
+	def template = libraryResource 'com/hastingsdirect/templates/Test3.groovy'
+	Template template=new Template()
+	
+}
