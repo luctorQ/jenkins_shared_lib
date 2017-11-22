fa63ef36b4bc990e5066bdd71992c66afb8da7c6
pluszynski <pluszynski@bleak.pl>
2017-11-22 14:31:34 +0000

    ds

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -2,12 +2,9 @@ import com.hastingsdirect.templates.Template
 
 def call(){
 	println 'nothing'
-	return 'abc'
-/*	def template = libraryResource 'com/hastingsdirect/templates/Test3.groovy'
-	
+	def template = libraryResource 'com/hastingsdirect/templates/Test3.groovy'	
 	println 'template:'+template
-	template=null;
-*///	Template template=new Template()
+///	Template template=new Template()
 	
 	
 }
