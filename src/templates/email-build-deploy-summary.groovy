17f542750246eb07b3e1ef35f50cec23ca2cdd2c
pluszynski <pluszynski@bleak.pl>
2017-11-24 13:28:38 +0000

    cleanup

--- src/templates/email-build-deploy-summary.groovy
+++ src/templates/email-build-deploy-summary.groovy
@@ -2,22 +2,9 @@ layout 'templates/layouts/layout-email.groovy',
 title: 'email',
 bodyContents: contents {
 
-	TEST?p('TEST exists'):p('no TEST')
-
-	//	APP_BUILD_DONE?h3('CREATED APPS'):p('No new crated builds')
 	APP_BUILD_DONE?
-			includeGroovy('templates/parts/createdApps.groovy')
+			includeGroovy('templates/parts/created-apps.groovy')
 			:p('No new applications')
 
-
-	/*	APP_BUILD_DONE.each{
-	 fragment 
-	 }
-	 pc: 26186 svn:Hastings/branches/CAD7/PolicyCenter/modules/configuration
-	 status:UNSTABLE
-	 build job: http://bx-cinappd03.network.uk.ad:8080/job/PC%20Build/609/
-	 JUnit Tests TOTAL:195 FAILED:7
-	 */	
-	p('This is the body')
-	p('new body')
+	p('rest of email')
 }
