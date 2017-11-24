1df4efa8df887d3a0ce1d942546f2d1b3bae1e53
pluszynski <pluszynski@bleak.pl>
2017-11-24 14:26:52 +0000

    dsds

--- src/com/hastingsdirect/templates/Template.groovy
+++ src/com/hastingsdirect/templates/Template.groovy
@@ -7,7 +7,7 @@ class Template implements Serializable {
 	
 	static String evaluate(String template,Map bindings=[:]) {
 		TemplateConfiguration config = new TemplateConfiguration();
-		MarkupTemplateEngine engine = new MarkupTemplateEngine(this.getClass().getClassLoader(),config);
+		MarkupTemplateEngine engine = new MarkupTemplateEngine(Template.class.getClassLoader(),config);
 		
 		def compiledTemplate =engine.createTemplateByPath(template).make(bindings)
 		return compiledTemplate.toString()
