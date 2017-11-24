9fcc033bc99c593acdc28a699fef3bf2d33ab441
pluszynski <pluszynski@bleak.pl>
2017-11-24 12:20:56 +0000

    sd

--- src/templates/parts/createdApps.groovy
+++ src/templates/parts/createdApps.groovy
@@ -1,10 +1,10 @@
 table{
 	thead{
 		tr{
-			th(class:'caption',colspan:7,'CREATED APPS')
+			th(class:'caption',colspan:8,'CREATED APPS')
 		}
 		tr{
-			th(colspan:4,'Builds Info')
+			th(colspan:5,'Builds Info')
 			th(colspan:3,'Tests Info')
 		}
 		tr{
@@ -12,10 +12,11 @@ table{
 			th('Rev')
 			th('Build')
 			th('Status')
+			th('Branch')
 			th('Total')
 			th('Failed')
 			th('Skipped')
-			th('Branch')
+			
 		}
 	}
 	tbody{
@@ -27,16 +28,10 @@ table{
 					a(href:b.build.jenkinsbuildurl,b.build.jenkinsbuildnumber)
 				}
 				td(b.build.buildresult)
+				td(b.build.trunk?'TRUNK':(b.build.svnpath =~ /Hastings\/branches\/(\w+)/)[1])
 				td(b.junittests.totalCount)
 				td(b.junittests.failCount)
-				td(b.junittests.skipCount)
-				def branchName=b.build.trunk?'TRUNK':null
-				if(!branchName) {
-					def group = (b.build.svnpath =~ /Hastings\/branches\/(\w+)/)
-					branchName=group[0]
-				}
-				
-				td(branchName)
+				td(b.junittests.skipCount)				
 			}
 		}
 	}
