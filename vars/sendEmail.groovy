8ee56458c75fbea5ef8b4070d25dc267f7c225da
pluszynski <pluszynski@bleak.pl>
2017-11-24 14:15:17 +0000

    gg

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,9 +1,5 @@
 import com.hastingsdirect.templates.Template
 
-import groovy.text.markup.MarkupTemplateEngine
-import groovy.text.markup.TemplateConfiguration
-
-
 def call(Map params=[template:null,
 			subject:'No subject',
 			recipients:'', //comma separated list of email addresses
