5614e5f9e4347f1e58f7566cfe07b4ad1eea7277
pluszynski <pluszynski@bleak.pl>
2017-11-24 14:05:56 +0000

    dsds

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -14,13 +14,13 @@ def call(Map params=[template:null,
 	def template=new Template(params.template)
 	try {
 		def body=template.evaluate(params.bindings)
-	/*	emailext(
+		emailext(
 				to: params.recipients,
 				replyTo: 'luchtort@gmail.com',
 				subject: params.subject,
 				attachmentsPattern: params.attachments,
 				body: body
-				)*/
+				)
 		eventsStore(
 			msg:"Email ${params.subject} sent to ${params.recipients}",
 			type:'EMAIL_SENT',
